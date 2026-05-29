17a5423c56c0c52f760c454294e496762a8e290d
Ren7482 <3548193006@qq.com>
2026-05-29 11:17:25 +0800

    3880. 两个值之间的最小绝对差值

---
+++ src/test/java/LeetCode_EnumRight/MinAbsoluteDifferenceTest.java
@@ -0,0 +1,34 @@
+package LeetCode_EnumRight;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MinAbsoluteDifferenceTest {
+    /*
+        示例 1：
+        输入： nums = [1,0,0,2,0,1]
+        输出： 2
+        解释：
+        有效下标对有：
+                (0, 3)，其绝对差为 abs(0 - 3) = 3。
+                (5, 3)，其绝对差为 abs(5 - 3) = 2。
+        因此，结果是 2。
+
+        示例 2：
+        输入： nums = [1,0,1,0]
+        输出： -1
+        解释：
+        数组中不存在有效下标对，因此结果是 -1。
+    */
+
+    private final MinAbsoluteDifference solution = new MinAbsoluteDifference();
+
+    @Test
+    public void testExample1() {
+        int[] nums = {1, 0, 0, 2, 0, 1};
+        int expected = 2;
+        int actual = solution.minAbsoluteDifference(nums);
+        assertEquals(expected, actual);
+    }
+}
