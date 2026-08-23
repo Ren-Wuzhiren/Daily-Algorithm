f691ff26a27ffd169fe8f42b362d9bb6ca691765
Ren7482 <3548193006@qq.com>
2026-08-23 16:02:09 +0800

    feat: 167 修正为双指针并 AC，登记打卡与 README（🟡 提示后 AC）

--- src/main/java/leetcode/editor/cn/TwoSumIiInputArrayIsSorted.java
+++ src/main/java/leetcode/editor/cn/TwoSumIiInputArrayIsSorted.java
@@ -8,13 +8,15 @@ public class TwoSumIiInputArrayIsSorted {
     class Solution {
         public int[] twoSum(int[] numbers, int target) {
             int right = numbers.length - 1, left = 0;
-            while(left < right) {
+            while (left < right) {
                 int sum = numbers[left] + numbers[right];
+                // 以后做算法题，把 sum == target 作为一种特殊情况处理。
                 if (sum == target) {
                     return new int[] {left + 1, right + 1};
                 } else if (sum < target) {
                     left++;
                 } else {
+                    // 不要把右指针自减条件 sum > target 漏掉
                     right--;
                 }
             }
@@ -31,6 +33,6 @@ public class TwoSumIiInputArrayIsSorted {
         int[] numbers = {2, 7, 11, 15};
         int target = 9;
         int[] ans = solution.twoSum(numbers, target);
-
+        System.out.println("ans = " + ans);
     }
 }
