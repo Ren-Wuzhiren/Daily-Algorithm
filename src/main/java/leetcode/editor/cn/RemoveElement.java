5fc417161717564ff1631d3e6429d74cb8736bb3
Ren7482 <3548193006@qq.com>
2026-08-23 15:27:06 +0800

    feat: 27 无提示重写成功（8.23 复习日）并登记打卡与 README

--- src/main/java/leetcode/editor/cn/RemoveElement.java
+++ src/main/java/leetcode/editor/cn/RemoveElement.java
@@ -12,10 +12,15 @@ public class RemoveElement {
                 if (nums[fast] != val) {
                     nums[slow] = nums[fast];
                     slow++;
+                    // fast++;
                 }
                 fast++;
+                // slow++;
             }
             return slow;
+            // return nums[slow];
+
+            // 不要把快慢指针位置写反，还有放回的内容应该是慢指针，而不是数组！
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
@@ -24,9 +29,23 @@ public class RemoveElement {
     public static void main(String[] args) {
         Solution solution = new RemoveElement().new Solution();
         // put your test code here
+        testExample1();
+        testExample2();
+    }
+
+    public static void testExample1() {
+        Solution solution = new RemoveElement().new Solution();
         int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
         int val = 2;
         int result = solution.removeElement(nums, val);
-        System.out.println(result);
+        System.out.println("result = " + result);
+    }
+
+    public static void testExample2() {
+        Solution solution = new RemoveElement().new Solution();
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+        int result = solution.removeElement(nums, val);
+        System.out.println("result = " + result);
     }
 }
